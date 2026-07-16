1172fa72c64ff6ca06202aff14b5c7c9c80c6141
AlexisPulido30 <alexispulido.agn@outlook.com>
2026-07-16 13:16:11 -0600

    feat: agregar datasource para tipo descripcion.

---
+++ src/domain/datasources/catalog/tipo-descripcion.datasource.ts
@@ -0,0 +1,26 @@
+import {CreateTipoDescripcionDto} from '../../dtos/catalog/create-tipo-descripcion.dto';
+import {UpdateTipoDescripcionDto} from '../../dtos/catalog/update-tipo-descripcion.dto';
+import { TipoDescripcionEntity } from '../../entities/catalog/tipo-descripcion.entity';
+
+export abstract class TipoDescripcionDatasource {
+    abstract create(
+        creatipoDescripcionDto: CreateTipoDescripcionDto,
+    ): Promise<TipoDescripcionEntity>
+
+    abstract getAll(): Promise<TipoDescripcionEntity[]>
+
+    abstract getById(
+        idTipoDescripcion: string,
+    ): Promise<TipoDescripcionEntity>
+    
+    abstract updateById(
+        idTipoDescripcion: string,
+        updateTipoDescripcionDto: UpdateTipoDescripcionDto,
+    ): Promise<TipoDescripcionEntity>
+
+    abstract deleteById(
+        idTipoDescripcion: string,
+    ): Promise<TipoDescripcionEntity>
+
+
+}
